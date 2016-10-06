a08c1190d75ef8be6efec61f4cbcce4777b18b2b
Jackson Rushing <j@jacksonrushing.com>
2016-10-05 23:08:53 -0400

    Updated m_controller
    Unhid lx, ly, rx, ry.
    Changed button names to be more intuitive

--- include/m_controller.h
+++ include/m_controller.h
@@ -1,37 +1,47 @@
 #pragma once
 #include <Windows.h>
 #include <Xinput.h>
-
 #include <vector>
+#define BUTTON_A XINPUT_GAMEPAD_A
+#define BUTTON_B XINPUT_GAMEPAD_B
+#define BUTTON_X XINPUT_GAMEPAD_X
+#define BUTTON_Y XINPUT_GAMEPAD_Y
+#define DPAD_LEFT XINPUT_GAMEPAD_DPAD_LEFT
+#define DPAD_RIGHT XINPUT_GAMEPAD_DPAD_RIGHT
+#define DPAD_UP XINPUT_GAMEPAD_DPAD_UP
+#define DPAD_DOWN XINPUT_GAMEPAD_DPAD_DOWN
+#define BUTTON_LB XINPUT_GAMEPAD_LEFT_SHOULDER
+#define BUTTON_RB XINPUT_GAMEPAD_RIGHT_SHOULDER
+#define BUTTON_LS XINPUT_GAMEPAD_LEFT_THUMB
+#define BUTTON_RS XINPUT_GAMEPAD_RIGHT_THUMB
+#define BUTTON_SELECT XINPUT_GAMEPAD_BACK
+#define BUTTON_START XINPUT_GAMEPAD_START
 static float degToRad = 3.14159f / 180.0f;
 static float radToDeg = 180.0f / 3.14159f;
 static int numControllers = 0;
 int getNextAvailableControllerId();
-struct stickInputs
-{
-public:
-	float	lX = 0.0f,
-		lY = 0.0f,
-		rX = 0.0f,
-		rY = 0.0f;
-};
+	
+		
+
 class MController
 {
 public:
 	MController() {}
 	MController(int id);
-	int index;
+	int		index;
 	XINPUT_STATE state;
-	void getInputs();
-	bool checkInputs(DWORD button);
-	bool isConnected();
+	void	getInputs();
+	bool	checkInputs(DWORD button);
+	bool	isConnected();
 	std::vector<DWORD> inputs;
-	stickInputs sticks;
-	stickInputs getSticks();
-	float getLeftStickAngle();
-	float getRightStickAngle();
-	float lT;
-	float rT;
+	float	getLeftStickAngle();
+	float	getRightStickAngle();
+	float	lT;
+	float	rT;
+	float	lX = 0.0f,
+			lY = 0.0f,
+			rX = 0.0f,
+			rY = 0.0f;
 
 
 };
--- src/m_controller.cpp
+++ src/m_controller.cpp
@@ -6,20 +6,20 @@
 
 DWORD mButtons[] =
 {
-	XINPUT_GAMEPAD_A,
-	XINPUT_GAMEPAD_B,
-	XINPUT_GAMEPAD_X,
-	XINPUT_GAMEPAD_Y,
-	XINPUT_GAMEPAD_DPAD_LEFT,
-	XINPUT_GAMEPAD_DPAD_RIGHT,
-	XINPUT_GAMEPAD_DPAD_UP,
-	XINPUT_GAMEPAD_DPAD_DOWN,
-	XINPUT_GAMEPAD_LEFT_SHOULDER,
-	XINPUT_GAMEPAD_RIGHT_SHOULDER,
-	XINPUT_GAMEPAD_LEFT_THUMB,
-	XINPUT_GAMEPAD_RIGHT_THUMB,
-	XINPUT_GAMEPAD_BACK,
-	XINPUT_GAMEPAD_START
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_X,
+	BUTTON_Y,
+	DPAD_LEFT,
+	DPAD_RIGHT,
+	DPAD_UP,
+	DPAD_DOWN,
+	BUTTON_LB,
+	BUTTON_RB,
+	BUTTON_LS,
+	BUTTON_RS,
+	BUTTON_SELECT,
+	BUTTON_START
 };
 int getNextAvailableControllerId()
 {
@@ -61,20 +61,20 @@ void MController::getInputs()
 	}
 	//Converts the stick inputs, by default a value between 0 and 32767 to a float between 0 and 1 for easier handling.
 	
-	this->sticks.lX = (float)state.Gamepad.sThumbLX / 32767;
-	this->sticks.lY = (float)state.Gamepad.sThumbLY / 32767;
-	this->sticks.rX = (float)state.Gamepad.sThumbRX / 32767;
-	this->sticks.rY = (float)state.Gamepad.sThumbRY / 32767;
+	this->lX = (float)state.Gamepad.sThumbLX / 32767;
+	this->lY = (float)state.Gamepad.sThumbLY / 32767;
+	this->rX = (float)state.Gamepad.sThumbRX / 32767;
+	this->rY = (float)state.Gamepad.sThumbRY / 32767;
 	//if sticks are within deadzone, zero out stick inputs.
-	if (abs(sticks.lX) < 0.24 && abs(sticks.lY) < 0.24)
+	if (abs(lX) < 0.24 && abs(lY) < 0.24)
 	{
-		sticks.lX = 0;
-		sticks.lY = 0;
+		lX = 0;
+		lY = 0;
 	}
-	if (abs(sticks.rX) < RIGHT_DEADZONE && abs(sticks.rY) < RIGHT_DEADZONE)
+	if (abs(rX) < RIGHT_DEADZONE && abs(rY) < RIGHT_DEADZONE)
 	{
-		sticks.rX = 0;
-		sticks.rY = 0;
+		rX = 0;
+		rY = 0;
 	}
 
 	//Converts the trigger inputs, by default a value between 0 and to a float between 0 and 1 for easier handling.
@@ -87,7 +87,6 @@ void MController::getInputs()
 */
 bool MController::checkInputs(DWORD button)
 {
-
 	if(std::find(this->inputs.begin(), this->inputs.end(), button)!= this->inputs.end())
 		return true;
 	else
@@ -106,20 +105,15 @@ bool MController::isConnected()
 		return false;
 	}
 }
-
-stickInputs MController::getSticks()
-{
-	return sticks;
-}
 float MController::getLeftStickAngle()
 {
 	float result;
-	result = radToDeg * atan2(this->sticks.lY, this->sticks.lX);
+	result = radToDeg * atan2(this->lY, this->lX);
 	return result;
 }
 float MController::getRightStickAngle()
 {
 	float result;
-	result = radToDeg * atan2(this->sticks.rY, this->sticks.rX);
+	result = radToDeg * atan2(this->rY, this->rX);
 	return result;
 }
